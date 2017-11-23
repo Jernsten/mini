2875bbb6ef5c89908202990026d2fc84bca98e26
Kemal Demirtas <demirtas@kth.se>
2017-11-23 17:10:28 +0100

    made scrolle effect and other things

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -26,18 +26,33 @@ function connect() {
 }
 
 function sendMessage() {
-    stompClient.send("/app/message", {}, JSON.stringify({'message': $("#message").val(), 'username': $("#username").val()}));
+    stompClient.send("/app/message", {}, JSON.stringify({
+        'message': $("#message").val(),
+        'username': $("#username").val()
+    }));
 }
 
 function showMessage(message) {
 //    $("#messages").append("<tr><td class=\"speaker\">" + JSON.parse(message.body).username + "</td><td class=\"speach\">" + JSON.parse(message.body).content + "</td></tr>");
 
-    var out = "<li class=\"other\"><div class='msg'>" +
-        "<div class='user'>"+JSON.parse(message.body).username+"</div>" +
-        "<p>"+JSON.parse(message.body).content+"</p>" +
-        "<time>...time...</time></div></li>";
+    console.log($("#name"));
+
+    if (document.getElementById('name').innerText == JSON.parse(message.body).username) {
+        var out = "<li class=\"self\"><div class='msg'>" +
+            "<div class='user'>" + JSON.parse(message.body).username + "</div>" +
+            "<p>" + JSON.parse(message.body).content + "</p>" +
+            "<time>...time...</time></div></li>";
+    } else {
+        var out = "<li class=\"other\"><div class='msg'>" +
+            "<div class='user'>" + JSON.parse(message.body).username + "</div>" +
+            "<p>" + JSON.parse(message.body).content + "</p>" +
+            "<time>...time...</time></div></li>";
+    }
 
     $("#messages").append(out);
+
+
+    $(".chat").animate({ scrollTop: $(document).height()}, 1000);
 }
 
 function disconnect() {
